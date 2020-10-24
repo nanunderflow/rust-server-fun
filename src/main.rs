bbff089ebc7dd306a7d312467cec97aae716357a
fitz <nanunderflow@gmail.com>
2020-10-23 22:16:06 -0400

    basic async client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+#[async_std::main]
+async fn main() -> surf::Result<()> {
+    let mut res = surf::get("https://bradfitzwater.com").await?;
+    dbg!(res.body_string().await?);
+    Ok(())
 }
